3938dea6ae7bd1740ef1725079ef92bd8811655c
Markno1 <marcotreglia0@gmail.com>
2018-05-23 17:48:35 +0200

    Fixed the CMakeList file and imported the CMake dependecies for SMFL lib

---
+++ GameROS/Asteroids-Multiplayer/build/CMakeFiles/game.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/mark/Github/Cpp/GameROS/Asteroids-Multiplayer/main.cpp" "/home/mark/Github/Cpp/GameROS/Asteroids-Multiplayer/build/CMakeFiles/game.dir/main.cpp.o"
+  "/home/mark/Github/Cpp/GameROS/Asteroids-Multiplayer/src/Asteroid.cpp" "/home/mark/Github/Cpp/GameROS/Asteroids-Multiplayer/build/CMakeFiles/game.dir/src/Asteroid.cpp.o"
+  "/home/mark/Github/Cpp/GameROS/Asteroids-Multiplayer/src/Bomb.cpp" "/home/mark/Github/Cpp/GameROS/Asteroids-Multiplayer/build/CMakeFiles/game.dir/src/Bomb.cpp.o"
+  "/home/mark/Github/Cpp/GameROS/Asteroids-Multiplayer/src/Bullet.cpp" "/home/mark/Github/Cpp/GameROS/Asteroids-Multiplayer/build/CMakeFiles/game.dir/src/Bullet.cpp.o"
+  "/home/mark/Github/Cpp/GameROS/Asteroids-Multiplayer/src/Player.cpp" "/home/mark/Github/Cpp/GameROS/Asteroids-Multiplayer/build/CMakeFiles/game.dir/src/Player.cpp.o"
+  "/home/mark/Github/Cpp/GameROS/Asteroids-Multiplayer/src/classes.cpp" "/home/mark/Github/Cpp/GameROS/Asteroids-Multiplayer/build/CMakeFiles/game.dir/src/classes.cpp.o"
+  "/home/mark/Github/Cpp/GameROS/Asteroids-Multiplayer/src/helping_functions.cpp" "/home/mark/Github/Cpp/GameROS/Asteroids-Multiplayer/build/CMakeFiles/game.dir/src/helping_functions.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../lib"
+  "."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ GameROS/Asteroids-Multiplayer/build/CMakeFiles/game.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/game.dir/main.cpp.o"
+  "CMakeFiles/game.dir/src/Player.cpp.o"
+  "CMakeFiles/game.dir/src/Bullet.cpp.o"
+  "CMakeFiles/game.dir/src/Bomb.cpp.o"
+  "CMakeFiles/game.dir/src/Asteroid.cpp.o"
+  "CMakeFiles/game.dir/src/classes.cpp.o"
+  "CMakeFiles/game.dir/src/helping_functions.cpp.o"
+  "game.pdb"
+  "game"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
