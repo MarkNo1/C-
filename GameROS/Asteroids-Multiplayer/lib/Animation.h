95462837df3b5989103994770e4d6018450775f5
JPRebocho <joaopaulopaiva@hotmail.com>
2018-05-28 15:41:32 +0200

    Animation class added

---
+++ GameROS/Asteroids-Multiplayer/lib/Animation.h
@@ -0,0 +1,14 @@
+//
+// Created by jrebocho on 28/05/18.
+//
+
+#ifndef ASTEROIDS_MULTIPLAYER_ANIMATION_H
+#define ASTEROIDS_MULTIPLAYER_ANIMATION_H
+
+
+class Animation {
+
+};
+
+
+#endif //ASTEROIDS_MULTIPLAYER_ANIMATION_H
---
+++ GameROS/Asteroids-Multiplayer/src/Animation.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jrebocho on 28/05/18.
+//
+
+#include "Animation.h"
