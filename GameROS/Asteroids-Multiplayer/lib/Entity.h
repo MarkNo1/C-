d250009ad7771f5b925c952d24e3fcd646e7a9dd
JPRebocho <joaopaulopaiva@hotmail.com>
2018-05-28 16:44:54 +0200

    Seperated Entity

---
+++ GameROS/Asteroids-Multiplayer/lib/Entity.h
@@ -0,0 +1,14 @@
+//
+// Created by jrebocho on 28/05/18.
+//
+
+#ifndef ASTEROIDS_MULTIPLAYER_ENTITY_H
+#define ASTEROIDS_MULTIPLAYER_ENTITY_H
+
+
+class Entity {
+
+};
+
+
+#endif //ASTEROIDS_MULTIPLAYER_ENTITY_H
